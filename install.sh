269626525fba66f0c04325fb09335f919c1b94df
Randall Box <rb@domainsvcs.com>
2020-04-11 23:54:03 -0500

    Fixed config file

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # Author : Randall Box
 
 echo "Installing..."
+cp config.example.php config.inc.php
 cp vendor/components/jquery/jquery.min.js web/libraries/jquery/
 cp vendor/twbs/bootstrap-sass/assets/javascripts/bootstrap.min.js web/libraries/bootstrap/
 sass scss/styles.scss > web/css/styles.css
-echo "Done.\n"
+echo "Done. Configure database in config.inc.php\n"
